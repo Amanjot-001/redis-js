c64e0fbf93bd63289ece7f86767544f03dff5b5e
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-03-25 17:18:03 +0530

    passive expiry in ms added

--- app/main.js
+++ app/main.js
@@ -24,17 +24,21 @@ const server = net.createServer((connection) => {
 
                 case 'set':
                     var keyStr = commands[1];
-                    const valueStr = commands[2];
-                    store.set(keyStr, valueStr);
+                    var valueStr = commands[2];
+                    let expiration = -1;
+                    if (commands.length > 4 && commands[3].toLowerCase() === 'px') {
+                        expiration = new Date().getTime() + parseInt(commands[4]);
+                    }
+                    store.set(keyStr, valueStr, expiration);
                     response = '+OK\r\n';
                     connection.write(response);
                     break;
 
                 case 'get':
                     var keyStr = commands[1];
-                    const value = store.get(keyStr);
-                    if (value !== undefined) {
-                        response = `$${value.length}\r\n${value}\r\n`;
+                    var object = store.get(keyStr);
+                    if (object !== null && (object.expiration === -1 || object.expiration > new Date().getTime())) {
+                        response = `$${object.value.length}\r\n${object.value}\r\n`;
                     } else {
                         response = '$-1\r\n';
                     }
--- app/storage.js
+++ app/storage.js
@@ -3,14 +3,24 @@ class Storage {
         this.store = {};
     }
 
-    set(key, value) {
-        this.store[key] = value;
-        console.log('store', this.store)
+    set(key, value, expiration) {
+        this.store[key] = {
+            value: value,
+            expiration: expiration
+        };
     }
 
     get(key) {
-        console.log('store', this.store)
-        return this.store[key];
+        const response = this.store[key];
+        if(response.expiration !== -1 && response.expiration < new Date().getTime()) {
+            this.delete(key);
+            return null;
+        }
+        return response;
+    }
+
+    delete(key) {
+        delete this.store[key];
     }
 }
 
