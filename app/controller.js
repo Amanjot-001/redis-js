19fbb3e27957d83b2482a76afe68cd5b781d8408
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-03-28 01:49:38 +0530

    added repl id and offset

--- app/controller.js
+++ app/controller.js
@@ -2,76 +2,79 @@ const Storage = require('./storage');
 const { wrongNoOfArgs, syntaxError } = require('./error')
 
 class Controller {
-    constructor() {
-        this.store = new Storage();
-    }
+	constructor() {
+		this.store = new Storage();
+	}
 
-    handleReq(commands, role) {
-        let response = '';
+	handleReq(commands, serverArgs) {
+		let response = '';
 
-        switch (commands[0].toLowerCase()) {
-            case 'ping':
-                if(commands.length > 2) {
-                    response = `-ERR ${wrongNoOfArgs(commands[0])}\r\n`;
-                    break;
-                }
-                response = commands[1] ? `+${commands[1]}\r\n` : '+PONG\r\n';
-                break;
+		switch (commands[0].toLowerCase()) {
+			case 'ping':
+				if (commands.length > 2) {
+					response = `-ERR ${wrongNoOfArgs(commands[0])}\r\n`;
+					break;
+				}
+				response = commands[1] ? `+${commands[1]}\r\n` : '+PONG\r\n';
+				break;
 
-            case 'echo':
-                if (commands.length > 2) {
-                    response = `-ERR ${wrongNoOfArgs(commands[0])}\r\n`;
-                    break;
-                }
-                const EchoStr = commands[1];
-                response = `$${EchoStr.length}\r\n${EchoStr}\r\n`;
-                break;
+			case 'echo':
+				if (commands.length > 2) {
+					response = `-ERR ${wrongNoOfArgs(commands[0])}\r\n`;
+					break;
+				}
+				const EchoStr = commands[1];
+				response = `$${EchoStr.length}\r\n${EchoStr}\r\n`;
+				break;
 
-            case 'set':
-                var keyStr = commands[1];
-                var valueStr = commands[2];
-                let expiration = -1;
-                if (commands.length > 4 && commands[3].toLowerCase() === 'px') {
-                    expiration = new Date().getTime() + parseInt(commands[4]);
-                }
-                this.store.set(keyStr, valueStr, expiration);
-                response = '+OK\r\n';
-                break;
+			case 'set':
+				var keyStr = commands[1];
+				var valueStr = commands[2];
+				let expiration = -1;
+				if (commands.length > 4 && commands[3].toLowerCase() === 'px') {
+					expiration = new Date().getTime() + parseInt(commands[4]);
+				}
+				this.store.set(keyStr, valueStr, expiration);
+				response = '+OK\r\n';
+				break;
 
-            case 'get':
-                if (commands.length > 2) {
-                    response = `-ERR ${wrongNoOfArgs(commands[0])}\r\n`;
-                    break;
-                }
-                var keyStr = commands[1];
-                var object = this.store.get(keyStr);
-                if (object !== undefined && (object.expiration === -1 || object.expiration > new Date().getTime())) {
-                    response = `$${object.value.length}\r\n${object.value}\r\n`;
-                } else {
-                    response = '$-1\r\n';
-                }
-                break;
+			case 'get':
+				if (commands.length > 2) {
+					response = `-ERR ${wrongNoOfArgs(commands[0])}\r\n`;
+					break;
+				}
+				var keyStr = commands[1];
+				var object = this.store.get(keyStr);
+				if (object !== undefined && (object.expiration === -1 || object.expiration > new Date().getTime())) {
+					response = `$${object.value.length}\r\n${object.value}\r\n`;
+				} else {
+					response = '$-1\r\n';
+				}
+				break;
 
-            case 'info':
-                var argument = commands[1].toLowerCase();
-                if (argument === 'replication') {
-                    const heading = '# Replication';
-                    const currentRole = `role:${role}`;
-                    response = `$${currentRole.length}\r\n${currentRole}\r\n`;
-                }
-                break;
+			case 'info':
+				var argument = commands[1].toLowerCase();
+				if (argument === 'replication') {
+					const heading = '# Replication';
+					const currentRole = `role:${serverArgs.role}`;
+					const replid = `master_replid:${serverArgs.master_replid}`;
+					const replOffset = `master_repl_offset:${serverArgs.master_repl_offset}`;
+					const totalLength = heading.length + currentRole.length + replid.length + replOffset.length + 6;
+					response = `$${totalLength}\r\n${heading}\r\n${currentRole}\r\n${replid}\r\n${replOffset}\r\n`;
+				}
+				break;
 
-            default:
-                const args = [];
-                for (let i = 1; i < commands.length; i++) {
-                    args.push(commands[i]);
-                }
-                response = `-ERR unknown command '${commands[0]}', with args beginning with: ${args.join(', ')}\r\n`;
-                break;
-        }
+			default:
+				const args = [];
+				for (let i = 1; i < commands.length; i++) {
+					args.push(commands[i]);
+				}
+				response = `-ERR unknown command '${commands[0]}', with args beginning with: ${args.join(', ')}\r\n`;
+				break;
+		}
 
-        return response;
-    }
+		return response;
+	}
 }
 
 module.exports = Controller;
---
+++ app/server.js
@@ -0,0 +1,34 @@
+class Server {
+	constructor() {
+		this.port;
+		this.masterHost;
+		this.masterPort;
+		this.role;
+		this.replid = this._generateReplicationID();
+		this.replOffset = 0;
+	}
+
+	_generateReplicationID() {
+		let id = '8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb';
+		return id;
+	}
+
+	extractArgs() {
+		const portIndex = process.argv.findIndex(e => e.includes('--port'));
+		this.port = portIndex !== -1 ? process.argv[portIndex + 1] : 6379;
+
+		const replicaIndex = process.argv.findIndex(e => e.includes('--replicaof'));
+		this.masterHost = replicaIndex !== 1 ? process.argv[replicaIndex + 1] : null;
+		this.masterPort = replicaIndex !== -1 ? process.argv[replicaIndex + 2] : null;
+		this.role = (this.masterHost && this.masterPort) ? 'slave' : 'master';
+
+		return {
+			port: this.port,
+			role: this.role,
+			master_replid: this.replid,
+			master_repl_offset: this.replOffset
+		}
+	}
+}
+
+module.exports = Server;
